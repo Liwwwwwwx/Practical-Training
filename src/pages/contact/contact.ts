d3a803856bc128b6ecd696aa3847035192ccc715
geshuyue1998 <1307378353@qq.com>
2018-12-10 10:50:17 +0800

    更新消息页面

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+//import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  imgurl:string;
+  @ViewChild('ac') ac;
+  icons:string="camera";
+  items = [];
+  //isActive=true;  
+  isActive=0;
+  isClick(i){
+    this.isActive=i;
+  }
+  arr=['通知','消息'];
   constructor(public navCtrl: NavController) {
 
   }
+
+  
 
 }
