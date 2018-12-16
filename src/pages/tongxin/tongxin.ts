8f01c27bd3cd857208cd029870b5376ced901cdf
liwei2016011717 <2335644296@qq.com>
2018-12-16 20:10:15 +0800

    更新了tongxin页面

--- src/pages/tongxin/tongxin.ts
+++ src/pages/tongxin/tongxin.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import {NavController, NavParams } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 /**
  * Generated class for the TongxinPage page.
  *
@@ -8,18 +8,28 @@ import { NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-//@IonicPage()
+// @IonicPage()
 @Component({
   selector: 'page-tongxin',
   templateUrl: 'tongxin.html',
 })
 export class TongxinPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  content;
+  constructor(public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TongxinPage');
+  }
+  goAbout(){
+    this.navCtrl.popToRoot();
+  }
+  ngOnInit(){
+    this.http.get('/notedata').subscribe(data => {
+      console.log(data);
+      this.content = data;
+      // console.log(this.content[0].userid)
+    })
   }
 
 }
