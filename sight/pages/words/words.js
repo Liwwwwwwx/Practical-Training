3ce16dbc6916771e252e3fdc79705d4e913cb5c9
马硕 <shou_ma.sjz@foxmail.com>
2019-05-29 11:25:33 +0800

    Add files via upload

--- sight/pages/words/words.js
+++ sight/pages/words/words.js
@@ -8,6 +8,53 @@ Page({
 
   },
 
+  //事件处理函数
+  goPic: function () {
+    var _this = this
+    wx.chooseImage({
+      count: 1, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+        wx.uploadFile({
+          url: 'https://sight.masure.cn/uploadRouter',
+          filePath: 'tempFilePaths[0]',
+          name: 'test',
+        })
+        _this.setData({
+          src: res.tempFilePaths
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+    console.log(this.picture)
+  },
+  
+  go(){
+    // 判断内容字数、
+    function wordsNum() {
+      return new Promise((resolve) => {
+        if (that.content == '') {
+          that.showToast('文章内容不能为空！');
+        } else if (that.content.length > 20) {
+          that.showToast('字数不能超过20字！');
+        } else {
+          resolve();
+        }
+      });
+    }
+
+
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
