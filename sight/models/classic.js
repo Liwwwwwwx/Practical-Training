054621397f27e649265c1ae837fc18e873eb5b52
Aluaead <1076843408@qq.com>
2019-05-29 09:23:38 +0800

    '实现navi组件开发

--- sight/models/classic.js
+++ sight/models/classic.js
@@ -1,50 +1,52 @@
-import {HTTP} from '../utils/util'
+import {
+  HTTP
+} from '../utils/util'
 
-class ClassicModel extends HTTP{
-    getLatest(){
-      return this.request({
-            url:'notes',
-          })
-    }
-  getClassic(index,nextOrPrevious){
-      console.log(index,nextOrPrevious)
-        return this.request({
-          url: 'notes/getnextorpre',
-          method:'POST',
-          data:{
-            NextOrPre:nextOrPrevious,
-            index:index
-          },
-         
-      })
-    }
-  
-  
-
-  isFirst(index){
-    let firstIndex = this._getLatestIndex()
-    console.log(index)
-    return firstIndex == index?true:false
+class ClassicModel extends HTTP {
+  getLatest() {
+    return this.request({
+      url: 'notes',
+    })
+  }
+  getClassic(index, nextOrPrevious) {
+    console.log(index, nextOrPrevious)
+    return this.request({
+      url: 'notes/getnextorpre',
+      method: 'POST',
+      data: {
+        NextOrPre: nextOrPrevious,
+        index: index
+      },
+    })
   }
 
-  isLatest(index){
-    return index == 1 ? true:false
+
+
+  isLatest(index) {
+    return index == 1 ? true : false
   }
 
-  _setLatestIndex(index){
-    wx.setStorageSync('latest',index)
+  isFirst(index) {
+    let latestIndex = wx.getStorageSync('latest')
+    console.log(latestIndex)
+    return latestIndex == index ? true : false
   }
 
-  _getLatestIndex(){
+
+  _setLatestIndex() {
     return this.request({
       url:'notes/maxindex'
     })
   }
 
-  _getKey(index){
-    let key = 'classic-'+index
-    return key
+  _getLatestIndex() {
+    this._setLatestIndex().then(res=>{
+      console.log(res.data[0].maxindex)
+      wx.setStorageSync('latest', res.data[0].maxindex)
+    })
   }
 
 }
-export {ClassicModel}
+export {
+  ClassicModel
+}
